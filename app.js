98ffca5b41fdb4fc03b7c9d0c1a15dede79ed087
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-06-28 00:23:55 -0500

    keyboard support

--- app.js
+++ app.js
@@ -31,27 +31,30 @@ new Vue({
         },
 
         prevPage: function () {
-            if (this.actualPage.page > 1) {
-                this.actualPage = this.pages[this.actualPage.page-2];
+            if (this.actualPage) {
+                if (this.actualPage.page > 1) {
+                    if (this.actualPage.page == this.pages.length && this.showAskNextChapter) {
+                        this.showAskNextChapter = false;
+                    } else {
+                        this.actualPage = this.pages[this.actualPage.page-2];
+                    }
+                }
             }
         },
 
         nextPage: function () {
-            // Dont add 1 because array start on 0
-            if (this.actualPage.page == this.pages.length) {
-                this.actualPage = {};
-                if (this.actualChapter != this.actualManga.chapters) {
-                    this.askForNextChapter();
+            if (this.actualPage) {
+                // Dont add 1 because array start on 0
+                if (this.actualPage.page == this.pages.length) {
+                    if (this.actualChapter != this.actualManga.chapters) {
+                        this.showAskNextChapter = true;
+                    } else {
+                        this.showMessageLastChapter = true;
+                    }
                 } else {
-                    this.showMessageLastChapter = true;
+                    this.actualPage = this.pages[this.actualPage.page];
                 }
-            } else {
-                this.actualPage = this.pages[this.actualPage.page];
             }
-        },
-
-        askForNextChapter: function () {
-            this.showAskNextChapter = true;
         },
 
         loadNextChapter: function () {
@@ -64,6 +67,19 @@ new Vue({
     },
 
     mounted: function () {
+        var vm = this;
+        window.addEventListener('keyup', function(event) {
+            var left = 37;
+            var right = 39;
+            var key = event.keyCode;
+
+            if (key == left) {
+                vm.prevPage();
+            } else if (key == right) {
+                vm.nextPage();
+            }
+        });
+
         this.$http.get('data/mangas.json').then(response => {
             this.mangas = response.body;
             this.actualManga = this.mangas[0];
