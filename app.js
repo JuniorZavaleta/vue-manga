25971d3c17275643b269939da3a4a2c5adc9fbcf
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-06-28 09:17:54 -0500

    update selected option

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ new Vue({
     data: {
         mangas: [],
         pages: [],
-        actualPage: {},
+        actualPage: 0,
         actualChapter: 0,
         actualManga: {},
         showAskNextChapter: false,
@@ -26,41 +26,33 @@ new Vue({
             }
         },
 
-        choosePage: function (event) {
-            this.actualPage = this.pages[event.target.value-1];
-        },
-
         prevPage: function () {
-            if (this.actualPage) {
-                if (this.actualPage.page > 1) {
-                    if (this.actualPage.page == this.pages.length && this.showAskNextChapter) {
-                        this.showAskNextChapter = false;
-                    } else {
-                        this.actualPage = this.pages[this.actualPage.page-2];
-                    }
+            if (this.actualPage > 1) {
+                if (this.actualPage == this.pages.length && this.showAskNextChapter) {
+                    this.showAskNextChapter = false;
+                } else {
+                    this.actualPage = this.pages[this.actualPage - 2].page;
                 }
             }
         },
 
         nextPage: function () {
-            if (this.actualPage) {
-                // Dont add 1 because array start on 0
-                if (this.actualPage.page == this.pages.length) {
-                    if (this.actualChapter != this.actualManga.chapters) {
-                        this.showAskNextChapter = true;
-                    } else {
-                        this.showMessageLastChapter = true;
-                    }
+            // Dont add 1 because array start on 0
+            if (this.actualPage == this.pages.length) {
+                if (this.actualChapter != this.actualManga.chapters) {
+                    this.showAskNextChapter = true;
                 } else {
-                    this.actualPage = this.pages[this.actualPage.page];
+                    this.showMessageLastChapter = true;
                 }
+            } else {
+                this.actualPage = this.pages[this.actualPage].page;
             }
         },
 
         loadNextChapter: function () {
             this.$http.get('data/one-piece-'+ (++this.actualChapter) +'.json').then(response => {
                 this.pages = response.body;
-                this.actualPage = response.body[0];
+                this.actualPage = response.body[0].page;
                 this.showAskNextChapter = false;
             });
         },
@@ -69,14 +61,16 @@ new Vue({
     mounted: function () {
         var vm = this;
         window.addEventListener('keyup', function(event) {
-            var left = 37;
-            var right = 39;
-            var key = event.keyCode;
+            if (event.target.tagName == "BODY") {
+                var left = 37;
+                var right = 39;
+                var key = event.keyCode;
 
-            if (key == left) {
-                vm.prevPage();
-            } else if (key == right) {
-                vm.nextPage();
+                if (key == left) {
+                    vm.prevPage();
+                } else if (key == right) {
+                    vm.nextPage();
+                }
             }
         });
 
